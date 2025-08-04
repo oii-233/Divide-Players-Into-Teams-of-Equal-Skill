8ee1cc4ce6245562162504f5facd1e050f6c12c0
oii-233 <imanibbb544@gmail.com>
2025-08-04 23:33:43 +0300

    Create .py

---
+++ .py
@@ -0,0 +1,17 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        left=0
+        right=len(skill)-1
+        target=skill[left]+skill[right]
+        sum=0
+        
+        while left<right:
+            if skill[left]+skill[right]!=target:
+                sum=-1
+                return sum
+            else:
+                sum+=skill[left]*skill[right]
+            left+=1
+            right-=1
+        return sum #in 20 min
